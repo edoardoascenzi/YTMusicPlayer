26e78a1fe775f339921a6fa237417c83860ff062
edoardoascenzi <edoardo.ascenzi97@gmail.com>
2024-05-13 16:54:00 +0200

    set up the yt url resolver set up the audio stream fatch through video id

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # YTMusicPlayer
 
 ### DISCLAIMER! This work is for academic purposes only. I just want to find a job in the software industry, so I am trying to do some projects to learn as much as possible.
+
+### Journal
+
+- The idea is to create a web app that works as a music player using youtube links as records. I will use Django, probably not the best choice, but I just want to learn how to use it.
+
+- The Youtube stream will be performed thorugh a YT API found on RAPID API (https://rapidapi.com/ytjar/api/yt-api). The free plan is limited to 500 request/day, that's okay for my purpose
--- YTMusicPlayer/YTStream/views.py
+++ YTMusicPlayer/YTStream/views.py
@@ -3,16 +3,91 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
+import requests
 
 
 @login_required(login_url='login')
 def index(request):
+    if request.method == 'POST':
+        YT_url = request.POST['YT_url']
+        video_id = resolve_url(YT_url)
+        if video_id == "":
+            messages.info(request, 'Youtube URL invalid')
+            return redirect('/')
+
+        url = "https://yt-api.p.rapidapi.com/dl"
+
+        querystring = {"id":video_id}
+
+        headers = {
+            "X-RapidAPI-Key": "0311a97011msh0728878f346d7d8p19c90ejsnce0feef60c8e",
+            "X-RapidAPI-Host": "yt-api.p.rapidapi.com"
+        }
+
+        response = requests.get(url, headers=headers, params=querystring)
+
+        if response.status_code != 200:
+            messages.info(request, 'Video not found')
+            return redirect('/')
+        response_data = response.json()
+        # print(response_data)
+
+        #maybe it is better to wrap all the following code inside a try and except
+        if response_data['status'] != 'OK': 
+            messages.info(request, 'Error in fatching the video')
+            return redirect('/')
+        
+        title = response_data.get('title', "")
+        lengthSeconds = response_data.get('lengthSeconds', 0)
+        channelTitle = response_data.get('channelTitle', "")
+        # adaptiveFormats is a list of dict -> inside the 21th item there is the url of the audio stream
+        audio_url = response_data.get('adaptiveFormats', [{}])[21].get('url',"")
+        print(title)
+        print(lengthSeconds)
+        print(channelTitle)
+        print(audio_url)
+
+        
+            
+        return redirect('music')
+        
+            
+
     return render(request, 'index.html')
+
+def resolve_url(url):
+    """
+    url -> Youtube video link
+    return "" if the format of the link is not correct
+    else extract the video ID
+    """
+    if 'www.youtube.com/watch?v=' not in url:
+        return ""
+    separator1 = url.find("=")
+    separator2 = url.find("&")
+    
+    if separator1 == -1:
+        # = key not found
+        return ""
+    
+    if separator2 != -1:
+        # & key found, so consider it
+        id = url[separator1+1:separator2]
+    else:
+        # & key not found, so take till the end
+        id = url[separator1+1:]
+
+    if len(id) != 11:
+        # id is either too long or too short
+        return ""
+    
+    return id
+    
 
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
-        password = request.POST["password"]
+        password = request.POST['password']
         print(email)
         print(password)
         user = auth.authenticate(username=username, password=password)
@@ -69,5 +144,6 @@ def logout(request):
     auth.logout(request)
     return redirect('login')
 
+@login_required(login_url='login')
 def music(request):
     return render(request, 'music.html')
