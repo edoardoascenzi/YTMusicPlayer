6739323e72a959a55ba230af971bdcb04bff520f
edoardoascenzi <edoardo.ascenzi97@gmail.com>
2024-05-15 22:17:40 +0200

    static file no debug

--- YTMusicPlayer/urls.py
+++ YTMusicPlayer/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.urls import re_path
+from django.views.static import serve
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('YTStream.urls')),
+
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
