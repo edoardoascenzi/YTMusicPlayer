4fc7c47652d00395da1a271b8becbe0ca8c83cd6
edoardoascenzi <edoardo.ascenzi97@gmail.com>
2024-05-15 14:47:24 +0200

    allowed host

--- YTMusicPlayer/settings.py
+++ YTMusicPlayer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hy&y_^0ie@=@3ig&*9o$r0$@jaz4890=*f4glaeqa&uk)0f^7v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','0.0.0.0']
 
 
 # Application definition
